5d48ef3a6503cf18c878d1c23cd55e980bd4cb01
H0onnn <dukei2017@naver.com>
2025-03-03 16:51:57 +0900

    style : tailwind config color token 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#805AF1",
+        grayf4: "#F4F4F4",
+      },
+    },
   },
   plugins: [],
 };
