a73789ac1b8025a743a391f1a2d1e5676ada2956
H0onnn <dukei2017@naver.com>
2025-03-04 23:46:35 +0900

    feat : 레이아웃 세팅

--- src/shared/components/layout/default-layout.tsx
+++ src/shared/components/layout/default-layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
-import { SideBar } from "./side-bar";
+import { SideBar, BottomNav } from "./nav";
+import { Header } from "./header";
 
 type LayoutProps = {
   children: ReactNode;
@@ -8,13 +9,19 @@ type LayoutProps = {
 export const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="min-h-screen bg-grayf4">
+      <Header />
+
       <div className="flex min-h-screen">
         <SideBar />
 
         <main className="flex-1 min-h-screen w-full md:ml-[200px]">
-          <div className="mx-auto w-full max-w-full px-4 py-6 md:max-w-[560px]">{children}</div>
+          <div className="mx-auto w-full max-w-full px-4 py-6 pb-20 md:pb-6 md:max-w-[560px]">
+            {children}
+          </div>
         </main>
       </div>
+
+      <BottomNav />
     </div>
   );
 };
